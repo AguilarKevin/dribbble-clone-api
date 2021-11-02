76c43ffd66c675a136eb2b275607370fa3ff3389
aguilarkevin <kevinaguilar0813@gmail.com>
2021-11-02 09:17:28 -0600

    some changes

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,6 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
 Route::post('/register', [AuthController::class, 'register']);
 
 Route::post('/login', [AuthController::class, 'login']);

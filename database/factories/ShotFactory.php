ff9fcf143972eae237942300e726139be85ac564
AguilarKevin <AguilarKevin@users.noreply.github.com>
2021-11-11 22:57:55 +0000

    Apply php-cs-fixer changes

--- database/factories/ShotFactory.php
+++ database/factories/ShotFactory.php
@@ -11,14 +11,15 @@ class ShotFactory extends Factory
 {
     protected $model = Shot::class;
 
-    #[ArrayShape(['title' => "string", 'description' => "string", 'views' => "int", 'saves' => "int", 'user_id' => "mixed"])] public function definition()
-    {
-        return [
+    #[ArrayShape(['title' => 'string', 'description' => 'string', 'views' => 'int', 'saves' => 'int', 'user_id' => 'mixed'])]
+ public function definition()
+ {
+     return [
             'title' => $this->faker->sentence(5),
             'description' => $this->faker->paragraph(6),
             'views'  => $this->faker->numberBetween(50, 7000),
             'saves'  => $this->faker->numberBetween(50, 70),
             'user_id' => $this->faker->randomElement(User::pluck('id')),
         ];
-    }
+ }
 }
